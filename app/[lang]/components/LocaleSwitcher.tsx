2d91bbeb479e6ed254bad8470b841e3da1837f42
Yash Godara <yash17godara@gmail.com>
2023-11-11 20:36:27 +0530

    frame motion | animated dropdown

--- app/[lang]/components/LocaleSwitcher.tsx
+++ app/[lang]/components/LocaleSwitcher.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 import { Icons } from './global/Icons';
 import { useLanguage } from '@/context/LanguageContext';
 import { i18n, getFlagIcon } from '@/i18n.config';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function LocaleSwitcher() {
   const pathName = usePathname();
@@ -31,21 +32,33 @@ export default function LocaleSwitcher() {
 
   return (
     <div className='relative'>
-      <button
+      <motion.button
         onClick={toggleDropdown}
+        // className='text-md flex h-[49px] w-[98px] flex-row items-center justify-center gap-2 px-3 py-2 text-center font-semibold uppercase text-white'
+        initial={{ scale: 1 }}
+        whileTap={{ scale: 0.9 }}
         className='text-md flex h-[49px] w-[98px] flex-row items-center justify-center gap-2 px-3 py-2 text-center font-semibold uppercase text-white'
       >
         {currentLocale}
         <span>{getFlagIcon(currentLocale)}</span>
         {dropdownIcon}
-      </button>
+      </motion.button>
+      <AnimatePresence>
+
+      
       {isDropdownOpen && (
-        <ul className='absolute left-0 top-full mt-1 w-[98px] rounded-md border bg-white font-semibold'>
+        <motion.ul 
+         // Add motion attributes for animation
+         initial={{ opacity: 0, y: -5 }}
+         animate={{ opacity: 1, y: 0 }}
+         exit={{ opacity: 0, y: -5 }}
+        className='absolute left-0 top-full mt-1 w-[98px] rounded-md border bg-white font-semibold'>
           {i18n.locales.map(locale => (
-            <li key={locale}>
+            <li key={locale}
+            >
               <a
                 href={redirectedPathName(locale)}
-                className='flex flex-row gap-2 px-4 py-2 text-sm text-black uppercase hover:bg-gray-100'
+                className='flex flex-row gap-2 px-4 py-2 text-sm text-black uppercase hover:bg-gray-200'
                 onClick={() => {
                   changeLang(locale);
                   setDropdownOpen(false);
@@ -56,8 +69,9 @@ export default function LocaleSwitcher() {
               </a>
             </li>
           ))}
-        </ul>
+        </motion.ul>
       )}
+      </AnimatePresence>
     </div>
   );
 }
