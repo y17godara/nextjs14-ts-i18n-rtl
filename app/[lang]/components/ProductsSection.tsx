0cdd8a937522a3f93b132c16cff86f6d82068381
Yash Godara <yash17godara@gmail.com>
2023-11-11 20:11:57 +0530

    products section

---
+++ app/[lang]/components/ProductsSection.tsx
@@ -0,0 +1,14 @@
+
+
+async function ProductsSection() {
+  // const { page } = await getDictionary(lang)
+  return (
+    <>
+      <section className='mt-16'>
+        <h1>{"Products"}</h1>
+      </section>
+    </>
+  )
+}
+
+export default ProductsSection
--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -1,34 +1,38 @@
-"use client";
-import React, { createContext, useContext, ReactNode } from 'react';
-import { Locale, i18n } from '@/i18n.config';
+'use client'
+import React, { createContext, useContext, ReactNode } from 'react'
+import { Locale, i18n } from '@/i18n.config'
 
 type LanguageContextProps = {
-  lang: Locale;
-  changeLang: (lang: Locale) => void;
-};
+  lang: Locale
+  changeLang: (lang: Locale) => void
+}
 
-const LanguageContext = createContext<LanguageContextProps | undefined>(undefined);
+const LanguageContext = createContext<LanguageContextProps | undefined>(
+  undefined
+)
 
-export const LanguageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [lang, setLang] = React.useState<Locale>('en'); // Set your default language here
+export const LanguageProvider: React.FC<{ children: ReactNode }> = ({
+  children
+}) => {
+  const [lang, setLang] = React.useState<Locale>('en') // Set your default language here
 
   const changeLang = (newLang: Locale) => {
     if (i18n.locales.includes(newLang)) {
-      setLang(newLang);
+      setLang(newLang)
     }
-  };
+  }
 
   return (
     <LanguageContext.Provider value={{ lang, changeLang }}>
       {children}
     </LanguageContext.Provider>
-  );
-};
+  )
+}
 
-export const useLanguage = () => {
-  const context = useContext(LanguageContext);
+export function useLanguage() {
+  const context = useContext(LanguageContext)
   if (!context) {
-    throw new Error('useLanguage must be used within a LanguageProvider');
+    throw new Error('useLanguage must be used within a LanguageProvider')
   }
-  return context;
-};
+  return context
+}
